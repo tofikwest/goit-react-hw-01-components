e1eabcb31166d0a2481bcecfcb7b5d9cca5e16f6
Tofik Hasanov <annexcies@gmail.com>
2021-07-18 19:01:19 +0400

    start hw

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Painting from "./components/Painting";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Главный компонент сайта</h1>
+    </div>
+  );
+};
+export default App;

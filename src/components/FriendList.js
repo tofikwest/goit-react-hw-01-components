f5a1e82d26ee15c335c44eda4d130f49234c6848
Tofik Hasanov <annexcies@gmail.com>
2021-07-18 21:39:30 +0400

    added PropType and defaultProps

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 const FriendList = ({ avatar, name, isOnline }) => {
   return (
     <li class="item">
@@ -7,6 +8,14 @@ const FriendList = ({ avatar, name, isOnline }) => {
       <p class="name">{name}</p>
     </li>
   );
+};
+FriendList.defaultProps = {
+  avatar:
+    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+};
+FriendList.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default FriendList;
